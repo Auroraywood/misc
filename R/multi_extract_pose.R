dfc576b534bc1a50f30f20060bea3d489cee249c
zhoudixin1996 <40650073+zhoudixin1996@users.noreply.github.com>
2019-05-09 15:38:48 +0800

    Update multi_extract_pose.R

--- R/multi_extract_pose.R
+++ R/multi_extract_pose.R
@@ -28,7 +28,9 @@ for(item in sub_sets){
   setwd(item)
   files <- dir() # set working dir
   if(dir.exists("tmp")){# if "tmp" folder has been created, "tmp" needed to be deleted from files
+    system("rm -r tmp")
     files <- setdiff(files, "tmp")
+    dir.create("tmp")
   }else{
     dir.create("tmp")
   }
@@ -44,8 +46,9 @@ for(item in sub_sets){
       tmp1 <- as.character(str_match(dat, "ENDMDL"))
       tmp2 <- which(tmp1=="ENDMDL")
       dat_out <- dat[2:(tmp2[1]-1)]
-      f_t <- unlist(str_split(f_n, "\\.")) # split
-      path <- paste0("./tmp/", f_t[1], "new", ".", f_t[2])
+      #f_t <- unlist(str_split(f_n, "\\.")) # split
+      #path <- paste0("./tmp/", f_t[1], ".", f_t[2])
+      path <- paste0("./tmp/", f_n)
       write.table(dat_out, file = path, quote = F, row.names = F, col.names = F)
     }, error=function(e){
       cat("ERROR :",f_n, "maybe has problem.", "\n")
